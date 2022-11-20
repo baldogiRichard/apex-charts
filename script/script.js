72efd8adef3c96df9f69d8aaa691380aa1e210dd
Báldogi Richárd <100072414+baldogiRichard@users.noreply.github.com>
2022-11-20 18:24:51 +0100

    Add files via upload
    v1

--- script/script.js
+++ script/script.js
@@ -2,4 +2,11 @@
 window.APEXCHARTS = window.APEXCHARTS || {};
 
 //Initialize plugin
-APEXCHARTS.initialize = function(config, data, init) {}
+APEXCHARTS.initialize = function(chartId,data,init) {
+
+    if (init && typeof init == 'function') init.call(this, data);
+
+    var chart = new ApexCharts(document.querySelector("#" + chartId), data);
+
+    chart.render();
+}
